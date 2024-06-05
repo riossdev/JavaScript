1b0fe417f169e7b5da4c360bc33f015e76e36e50
Juan Rios <jriossdev@gmail.com>
2024-06-05 16:55:07 -0500

    Secund Commit test

---
+++ js_courses/Eloquent_JavaScript/index.js
@@ -0,0 +1,15 @@
+// let total = 0, cuenta = 1
+// while (cuenta <= 10){
+//   total += cuenta
+//   cuenta += 1
+// }
+
+// console.log(total)
+
+const name = {
+  name: "juan",
+  lastname : "rios"
+}
+
+// name.correo = "jrioss@mail.com" 
+console.log(name)
